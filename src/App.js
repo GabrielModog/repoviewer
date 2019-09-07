0324b4e5159f53e0c324c1a472048f46b2ddefec
Mochorro <gabrielmodog@gmail.com>
2019-09-06 22:22:16 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+function createID(){
+	return Number(Math.floor([Math.floor(Math.random() * 10),
+			  Math.floor(Math.random() * 10),
+			  Math.floor(Math.random() * 10),
+			  Math.floor(Math.random() * 10)].join("")/10));
 }
+*/
 
-export default App;
+export default function App(){
+
+	/*	
+	const [repositories, setRepositories] = React.useState([
+		{id: 1, name: "repo-1"},
+		{id: 2, name: "repo-2"},
+		{id: 3, name: "repo-3"}
+	]);
+	*/
+	
+	const [repositories, setRepositories] =  useState([]);
+	
+	/*
+	function handleAddRepo(){
+		setRepositories([...repositories, 
+							{id: createID(),
+							name: "repo-" + createID()}]);
+	}
+	*/
+	
+	useEffect( async () => {
+		
+		const response = await fetch('https://api.github.com/users/torvalds/repos');
+		const data = await response.json();
+		
+		setRepositories(data);
+		
+  }, []);
+  
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Did you have ${filtered.length} fave on you desk?`;  
+  }, [repositories]);
+
+  function handleFave(id){
+    const newRepo = repositories.map(repo =>{
+      return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo;
+    });
+
+    setRepositories(newRepo);
+  }
+	
+	return(
+		<>
+		<ul>
+
+			{repositories.map(repo => (
+				<li key = { repo.id }>
+          { repo.name }
+          { repo.favorite && <span> (Fave)</span>}
+          <button onClick = {() => handleFave(repo.id)}>Fave</button>
+				</li>
+      ))}
+      
+		</ul>
+		</>
+	);
+}
